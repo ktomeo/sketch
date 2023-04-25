80ce636a77d60f9219f7f3de0aa50d372c09734d
Kai Tomeo <kaitomeo14@gmail.com>
2023-04-24 21:52:30 -0700

    Boxes can change size depending on entry

--- javascript.js
+++ javascript.js
@@ -1,11 +1,15 @@
 const container = document.querySelector('#container');
 var grid = document.querySelectorAll('.grid');
 
-let size = 16
+let size = 29
+let boxWidth = 500 / size
+let path = `width: ${boxWidth}px; height: ${boxWidth}px`
 
 for (let i = 0; i < (size * size) - 1; i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
     container.appendChild(grid);
-    grid.setAttribute('style', 'width: 5px; height: 5px');
+    grid.setAttribute("style", path);
 }
+
+console.log(path)
