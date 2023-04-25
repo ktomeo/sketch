fd6cbd0b45b6a58c056a512d029b13123ceaa5db
Kai Tomeo <kaitomeo14@gmail.com>
2023-04-24 22:34:46 -0700

    Fixed the last box not coloring in

--- javascript.js
+++ javascript.js
@@ -2,19 +2,17 @@ const container = document.querySelector('#container');
 var grid = document.querySelectorAll('.grid');
 const gridFill = document.getElementsByClassName("grid");
 
-let size = 40
+let size = 5
 let boxWidth = 500 / size
 
-for (let i = 0; i < (size * size) - 1; i++) {
+for (let i = 0; i < (size * size); i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
     container.appendChild(grid);
     grid.setAttribute("style", `width: ${boxWidth}px; height: ${boxWidth}px`);
     gridFill[i].addEventListener("mouseover", (event) => {
         event.target.style.backgroundColor = "black";
-    }, 
-    false
-    )
+    })
 }
 
 
