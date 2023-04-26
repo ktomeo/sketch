0f24d701ca18f3c672be9d5ab7f27773bc1978ff
Kai Tomeo <kaitomeo14@gmail.com>
2023-04-25 21:51:00 -0700

    Sketch can be controlled with two buttons

--- javascript.js
+++ javascript.js
@@ -2,19 +2,29 @@ const container = document.querySelector('#container');
 var grid = document.querySelectorAll('.grid');
 const gridFill = document.getElementsByClassName("grid");
 
-let size = prompt('Set grid width')
-let boxWidth = 500 / size
 
-for (let i = 0; i < (size * size); i++) {
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    container.appendChild(grid);
-    grid.setAttribute("style", `width: ${boxWidth}px; height: ${boxWidth}px`);
-    gridFill[i].addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = "black";
-    });
+
+function startNew() {
+    let size = prompt('Set grid width');
+    let boxWidth = 500 / size
+    if (size >= 100) {
+        alert('Error. Please return a number smaller than 100.')
+    }  else if (size < 100) {
+            for (let i = 0; i < (size * size); i++) {
+                const grid = document.createElement('div');
+                grid.classList.add('grid');
+                container.appendChild(grid);
+                grid.setAttribute("style", `width: ${boxWidth}px; height: ${boxWidth}px`);
+                gridFill[i].addEventListener("mouseover", (event) => {
+                    event.target.style.backgroundColor = "black";
+                });
+            }
+        }
 }
 
-
-
-
+function removeElementsbyClass(){
+    const elements = document.getElementsByClassName("grid");
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
